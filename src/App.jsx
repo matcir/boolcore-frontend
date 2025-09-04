344d236b6edfd6c5b2d33fcfa85fa0d6b6ea9b7c
simone-poli <polismn97@gmail.com>
2025-09-04 16:24:33 +0200

    install and init react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Router, Route } from "react-router-dom"
+
 
 export default function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <BrowserRouter>
 
+        <Router>
+
+          <Route path="/" element="" />
+          <Route path="/products" element="" />
+          <Route path="/products/:id" element="" />
+
+          
+
+
+
+        </Router>
+
+
+
+      </BrowserRouter>
     </>
   )
 }
