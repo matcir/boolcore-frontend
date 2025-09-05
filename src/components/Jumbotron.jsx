ccc26308fbae7312569e634b91f5011ef357aa62
GianniC88 <gianni.colucci.88@gmail.com>
2025-09-05 18:33:55 +0200

    Aggiorna il titolo e la descrizione nel componente Jumbotron; modifica il colore di sfondo e la dimensione del testo

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,8 +1,8 @@
 export default function Jumbotron({ title, description }) {
 	return <>
 		<div className="jumbotron mt-5 p-5 bg-color-jumbo  rounded-4">
-			<h1 className="display-4 acid-text">{title}</h1>
-			<p className="leadn acid-text">{description}</p>
+			<h1 className="display-3 acid-text">{title}</h1>
+			<p className=" acid-text">{description}</p>
 		</div>
 	</>
 }
