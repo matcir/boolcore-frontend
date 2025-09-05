539543b81c0a001b32f696741a642b51ba56168d
matcir <matteocirilloboolean@gmail.com>
2025-09-05 17:34:25 +0200

    add data context

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,24 @@ import SingleProduct from "./pages/SingleProduct"
 import Navbar from "./components/Navbar"
 import DefaultLayout from "../Layout/DefaultLayout"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { DataProvider } from "./contexts/DataContext"
+import { LoaderProvider } from "./contexts/LoaderContext"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductsList />} />
-          <Route path="/products/:id" element={<SingleProduct />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LoaderProvider>
+      <DataProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/products" element={<ProductsList />} />
+              <Route path="/products/:id" element={<SingleProduct />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </DataProvider>
+    </LoaderProvider>
   )
 }
